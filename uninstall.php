19cabcc0b95bb3ba1914e6d160f9a98a20e6bdc8
RedSand <RedSand@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-25 07:20:30 +0000

    Version 1.9.3. Please see changelog for details.
    git-svn-id: https://plugins.svn.wordpress.org/wp-spamshield/tags/1.9.3@1187333 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ uninstall.php
@@ -0,0 +1,24 @@
+<?php
+/*
+WP-SpamShield - uninstall.php
+Version: 1.9.3
+
+This script uninstalls WP-SpamShield and removes all options and traces of its existence.
+*/
+
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit(); }
+
+function spamshield_uninstall_plugin() {
+	/* Delete Options */
+	$del_options = array( 'wp_spamshield_version', 'spamshield_options', 'spamshield_widget_settings', 'spamshield_last_admin', 'spamshield_admins', 'spamshield_admin_notices', 'spamshield_count', 'spamshield_reg_count', 'spamshield_procdat', 'spamshield_install_status', 'spamshield_warning_status', 'spamshield_regalert_status', 'spamshield_nonces', 'spamshield_ubl_cache', 'spamshield_ubl_cache_disable', 'spamshield_wpssmid_cache', 'spamshield_whitelist_keys', 'ak_count_pre', 'spamshield_init_user_approve_run' );
+	foreach( $del_options as $i => $option ) { delete_option( $option ); }
+	/* Delete User Meta */
+	$del_user_meta = array( 'wpss_user_ip', 'wpss_admin_status', 'wpss_new_user_approved', 'wpss_new_user_email_sent' );
+	$user_ids = get_users( array( 'blog_id' => '', 'fields' => 'ID' ) );
+	foreach ( $user_ids as $user_id ) { foreach( $del_user_meta as $i => $key ) { delete_user_meta( $user_id, $key ); } }
+	}
+
+spamshield_uninstall_plugin();
+
+/* "Then it's time I disappear..." */
+?>
